ec540cf4e2b9a1e73fa17de4e32b81c4cca32e27
dragiPan <dtm.pantovic@gmail.com>
2025-03-09 21:51:16 +0100

    Homepage Made

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBV1XuoNeBpRlM5yo5MLWrymkWNRc8v0KE",
-  authDomain: "mxd-homepage.firebaseapp.com",
-  projectId: "mxd-homepage",
-  storageBucket: "mxd-homepage.firebasestorage.app",
-  messagingSenderId: "613010133696",
-  appId: "1:613010133696:web:f5c273c9f43c5209675325",
-  measurementId: "G-0D4NHFR5D8"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
